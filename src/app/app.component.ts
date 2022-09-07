6fcfb0faae1b429a66fd20fdc6f75dc6aa3294a8
Arga T. Wijaya <arga173@yahoo.com.sg>
2022-09-07 22:55:28 +0800

    day32AM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { TodoComponent } from './components/todo.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'day32';
+  value: number = 0
+  numList: number[] = []
+
+  @ViewChild(TodoComponent)
+  todoComp!: TodoComponent
+
+  @ViewChild('todo')
+  todoElemRef!: ElementRef
+
+  @ViewChildren(TodoComponent)
+  todoComps!: QueryList<TodoComponent>
+
+  ngOnInIt(): void {
+    console.info("onInIt todoComp:: ",this.todoComp)
+
+    console.info("onInIt activated", this.numList)
+    for (let i = 0; i < 5; i++) {
+      this.numList.push(Math.floor(Math.random() * 100))
+    }
+    console.info("onInIt random:: ",this.numList)
+  }
+  ngAfterViewInIt(): void {
+    console.info('>>> ngAfterViewInit todoComp:', this.todoComp)
+    console.info('>>> ngAfterViewInit todoElemRef:', this.todoElemRef.nativeElement)
+    console.info('>>> ngAfterViewInit todoComps:', this.todoComps.length)
+  }
+
+  saveToDo() {
+    const todo = this.todoComp.getValues()
+    console.info("saveToDo:: ", todo)
+
+  }
+
+  randomize() {
+    this.value = Math.floor(Math.random() * 100)
+    // create new array
+    const tmp: number[] = [...this.numList, this.value]
+    // assign new array to binding
+    this.numList = tmp
+    console.info("randomize() set:: ",tmp)
+  }
 }
